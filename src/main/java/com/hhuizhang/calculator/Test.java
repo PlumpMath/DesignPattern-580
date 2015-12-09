97088d138c3a6b787c94e7c896e616ba76999a2b
hhuizhang <cathyzh8807@gmail.com>
2015-12-10 02:10:10 +0800

    add Test.java

---
+++ src/main/java/com/hhuizhang/calculator/Test.java
@@ -0,0 +1,14 @@
+package com.hhuizhang.calculator;
+
+
+
+/**
+ *
+ * Created by catherine.zh on 2015/12/10.
+ */
+public class Test {
+
+    public static void main(String [] agrs){
+        System.out.println("Hello world");
+    }
+}
